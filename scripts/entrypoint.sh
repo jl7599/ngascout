55a63681d55107cc06d699ab152f7b9d0d7f8ea6
jlei <819178421@qq.com>
2026-05-21 16:43:05 +0800

    fix: fix 'not command uv' in /bin/sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
 set -e
 INTERVAL=${CRON_INTERVAL:-5}
-echo "*/$INTERVAL * * * * cd /app && uv run python -m src.main >> /proc/1/fd/1 2>> /proc/1/fd/2" > /etc/cron.d/nga
+echo "*/$INTERVAL * * * * cd /app && /usr/local/bin/uv run python -m src.main >> /proc/1/fd/1 2>> /proc/1/fd/2" > /etc/cron.d/nga
 chmod 0644 /etc/cron.d/nga
 crontab /etc/cron.d/nga
-
 
 echo "Cron job set up:"
 crontab -l
